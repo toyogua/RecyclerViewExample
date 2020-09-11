a218747c23f85e17dfa63a0d89bd9288715b33ee
JOSE RAMIREZ <slipknotoy@hotmail.com>
2020-09-11 17:35:32 -0600

    Add adapter

---
+++ app/src/main/java/com/example/recyclerviewexample/MainAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.recyclerviewexample
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_row.view.*
+
+class MainAdapter(private val context: Context): RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
+
+    private var dataList = mutableListOf<Usuario>()
+
+    fun setListData( data: MutableList<Usuario>){
+        dataList = data
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_row, parent, false)
+        return MainViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        val user = dataList[position]
+        holder.bindView(user)
+    }
+
+    override fun getItemCount(): Int {
+        if (dataList.size > 0 ){
+            return dataList.size
+        }else{
+            return 0
+        }
+    }
+
+    inner class MainViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        fun bindView( user: Usuario){
+            Glide.with(context).load(user.imageUrl).into(itemView.circleImageView)
+            itemView.txt_title.text = user.nombre
+            itemView.txt_desc.text = user.descripcion
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/recyclerviewexample/Usuario.kt
@@ -0,0 +1,5 @@
+package com.example.recyclerviewexample
+
+data class Usuario( val imageUrl: String = "DEFAULT URL",
+                    val nombre: String = "DEFAULT NAME",
+                    val descripcion: String = "DEFAULT DESC")
